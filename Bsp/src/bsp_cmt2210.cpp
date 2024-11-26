f2e27b0c240095483a2fff0394da8c2d92f81601
haliwt <jonewang@126.com>
2024-11-26 19:30:19 +0800

    update get 315MHZ synchronization single .

--- Bsp/src/bsp_cmt2210.cpp
+++ Bsp/src/bsp_cmt2210.cpp
@@ -38,36 +38,11 @@ static void sync_single(void)
          
           high_rc=0;
          low_rc++;
-         if(low_rc > 200) low_rc = 0; //200*10us = 500us
+        // if(low_rc > 200) low_rc = 0; //200*10us = 500us
 
 
     }
-    else if(RF_KEY_GetValue()==1 && (  low_rc < 90 && low_rc >30) && sync_start_flag ==0 &&  high_end_flag ==0){
-         low_start_flag =1;
-        
-
-         high_rc++;
-
-         if(high_rc > 50)high_rc=0;
-           
-           
-    }
-    else if(RF_KEY_GetValue()==1 && low_start_flag==0){
-       low_rc=0;
-    }
-
-
-    
-
-    if(RF_KEY_GetValue()==0 && (high_rc <  90 && high_rc > 30)  && sync_start_flag ==0 && low_start_flag ==1){
-           high_end_flag= 1;
-          low_rc++;
-     
-
-          if(low_rc > 1500) low_rc =0; //1500x 10us = 15000us =15ms
-
-     }
-     else if(RF_KEY_GetValue()==1 &&   (low_rc <= 1500 && low_rc >700) && sync_start_flag ==0 &&  high_end_flag== 1){
+    if(RF_KEY_GetValue()==1 &&   (low_rc <= 1500 && low_rc >700) && sync_start_flag ==0){
              high_end_flag=2;
                
 
@@ -87,7 +62,7 @@ static void sync_single(void)
 
 
       }
-      else if(RF_KEY_GetValue()==1 &&   (low_rc > 1500 || low_rc  < 700) && sync_start_flag ==0 &&  high_end_flag== 1){
+      else if(RF_KEY_GetValue()==1 &&   (low_rc > 1500 || low_rc  < 700) && sync_start_flag ==0){
 
          low_rc =0;
 
