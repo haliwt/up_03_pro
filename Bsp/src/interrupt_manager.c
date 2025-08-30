c982aafe81fb6ffbfb1e4c4496a38a44a424fe03
haliwt <jonewang@126.com>
2025-08-30 16:48:23 +0800

    UPDATE RECEIVE data is correct is success .DATA.2025.08.30

--- Bsp/src/interrupt_manager.c
+++ Bsp/src/interrupt_manager.c
@@ -1,7 +1,7 @@
 #include "bsp.h"
 
-#define SYNC_MIN_US   6000    // 同步信号�?小时�?(us)
-#define SYNC_MAX_US   9000     // 同步信号�?大时�?(us)
+#define SYNC_MIN_US   3000    // 同步信号�?小时�?(us)
+#define SYNC_MAX_US   10000     // 同步信号�?大时�?(us)
 
 #define BIT0_MIN_US   100     // 0位最大时�?(us)
 #define BIT0_MAX_US   400//350     //  0位最大时�?(us)
