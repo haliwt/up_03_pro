d8b2b2bfe966cb2cb3f3f574883780daf3b749c8
haliwt <jonewang@126.com>
2025-11-10 20:53:12 +0800

    INPUT KEY MUST up or down ,don't NG .is bug.

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -84,7 +84,7 @@ void MX_GPIO_Init(void)
   /**/
   GPIO_InitStruct.Pin = KEY_POWER_Pin;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP; //WT.EDIT 2025.11.10
   LL_GPIO_Init(KEY_POWER_GPIO_Port, &GPIO_InitStruct);
 
 }
